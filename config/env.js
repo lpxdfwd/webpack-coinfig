1fd4fae80b45ee0d5d6ea33e9a72b114e3903b0a
liupeixin <920331287@qq.com>
2019-08-17 12:00:13 +0800

    config

---
+++ config/env.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const rootPath = fs.realpathSync(process.cwd());
+const envDir = 'environment';
+const suffix = '.env.js';
+const NODE_ENV = process.env.NODE_ENV;
+
+const defaultEnv = {
+  NODE_ENV
+};
+
+let envConfig = null;
+
+function loadEnv () {
+  if (!envConfig) {
+    const envKey = process.argv[2] || NODE_ENV;
+    const envFile = path.join(rootPath, envDir, envKey + suffix);
+    const envObj = fs.existsSync(envFile) ? {...defaultEnv, ...require(envFile)} : {...defaultEnv};
+    envConfig = getClientEnvironment(envObj);
+  }
+  return envConfig;
+}
+
+function getClientEnvironment (envObj) {
+  const stringfyEnvs = Object.create(null);
+  const keys = Object.keys(envObj);
+  for (let i = 0, len = keys.length; i < len; i++) {
+    const key = keys[i];
+    stringfyEnvs[key] = JSON.stringify(envObj[key]);
+  }
+
+  return {
+    raw: envObj,
+    stringified: {
+      'process.env': stringfyEnvs
+    }
+  }
+}
+
+module.exports = loadEnv;
---
+++ config/webpack.config.dev.js
@@ -0,0 +1,231 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const PnpWebpackPlugin = require('pnp-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
+const paths = require('./paths');
+const getClientEnvironment = require('./env');
+const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
+
+const postcssNormalize = require('postcss-normalize');
+
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const env = getClientEnvironment();
+
+const getStyleLoaders = (cssOptions, preProcessor) => {
+  const loaders = [
+    require.resolve('style-loader'),
+    {
+      loader: require.resolve('css-loader'),
+      options: cssOptions,
+    },
+    {
+      loader: require.resolve('postcss-loader'),
+      options: {
+        ident: 'postcss',
+        plugins: () => [
+          require('postcss-flexbugs-fixes'),
+          require('postcss-preset-env')({
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+          }),
+          postcssNormalize(),
+        ],
+        sourceMap: false,
+      },
+    },
+  ].filter(Boolean);
+  if (preProcessor) {
+    loaders.push({
+      loader: require.resolve(preProcessor),
+      options: {
+        sourceMap: false,
+      },
+    });
+  }
+  return loaders;
+};
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: [
+    paths.poliffyIndexJs,
+    require.resolve('react-dev-utils/webpackHotDevClient'),
+    paths.appIndexJs,
+  ].filter(Boolean),
+  output: {
+    pathinfo: true,
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    publicPath: env.raw.publicPath,
+    devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
+    runtimeChunk: true,
+  },
+
+  resolve: {
+    extensions: paths.moduleFileExtensions
+      .map(ext => `.${ext}`),
+    plugins: [
+      PnpWebpackPlugin,
+      new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+    ],
+  },
+  resolveLoader: {
+    plugins: [
+      PnpWebpackPlugin.moduleLoader(module),
+    ],
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      { parser: { requireEnsure: false } },
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              formatter: require.resolve('react-dev-utils/eslintFormatter'),
+              eslintPath: require.resolve('eslint'),
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+        include: paths.appSrc,
+      },
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: 10000,
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+          {
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            include: paths.appSrc,
+            loader: require.resolve('babel-loader'),
+            options: {
+              customize: require.resolve(
+                'babel-preset-react-app/webpack-overrides'
+              ),
+              cacheDirectory: true,
+              cacheCompression: true,
+              compact: true,
+            },
+          },
+          {
+            test: /\.(js|mjs)$/,
+            exclude: /@babel(?:\/|\\{1,2})runtime/,
+            loader: require.resolve('babel-loader'),
+            options: {
+              babelrc: false,
+              configFile: false,
+              compact: false,
+              presets: [
+                [
+                  require.resolve('babel-preset-react-app/dependencies'),
+                  { helpers: true },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: true,
+              sourceMaps: false,
+            },
+          },
+          {
+            test: cssRegex,
+            exclude: cssModuleRegex,
+            use: getStyleLoaders({
+              importLoaders: 1,
+              sourceMap: false,
+            }),
+            sideEffects: true,
+          },
+          {
+            test: cssModuleRegex,
+            use: getStyleLoaders({
+              importLoaders: 1,
+              sourceMap: false,
+              modules: true,
+              getLocalIdent: getCSSModuleLocalIdent,
+            }),
+          },
+          {
+            test: /\.less$/,
+            loader: getStyleLoaders(
+              {
+                importLoaders: 2,
+              },
+              'less-loader'
+            ),
+            sideEffects: true,
+          },
+          {
+            loader: require.resolve('file-loader'),
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: paths.appHtml,
+    }),
+    new ModuleNotFoundPlugin(paths.appPath),
+    new webpack.DefinePlugin(env.stringified),
+    new webpack.HashedModuleIdsPlugin({hashDigest: 'hex'}),
+    new webpack.HotModuleReplacementPlugin(),
+    new CaseSensitivePathsPlugin(),
+    new WatchMissingNodeModulesPlugin(paths.appNodeModules),
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+      publicPath: env.raw.publicPath,
+      generate: (seed, files) => {
+        const manifestFiles = files.reduce(function(manifest, file) {
+          manifest[file.name] = file.path;
+          return manifest;
+        }, seed);
+        return {
+          files: manifestFiles,
+        };
+      },
+    }),
+    //防止moment.js中的语言包被打包
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ].filter(Boolean),
+  node: {
+    module: 'empty',
+    dgram: 'empty',
+    dns: 'mock',
+    fs: 'empty',
+    http2: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  },
+  performance: false,
+};
